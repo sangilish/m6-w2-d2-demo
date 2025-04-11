464d843e47e190ae2da86ce2f603eea7a026f406
sangilish <56685007+sangilish@users.noreply.github.com>
2025-04-11 15:38:13 -0700

    보안 문제가 있는 SSH 키 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Header from './features/header/Header';
+import TodoList from './features/todos/TodoList';
+import Footer from './features/footer/Footer';
+import './App.css';
 
 function App() {
   return (
@@ -6,17 +10,20 @@ function App() {
       <nav>
         <section>
           <h1>Redux Fundamentals Example</h1>
-
-          <div className="navContent">
-            <div className="navLinks"></div>
-          </div>
         </section>
       </nav>
-      <section>
-        <h2>Welcome to the Redux Fundamentals example app!</h2>
-      </section>
+      <main>
+        <section className="medium-container">
+          <h2>Todos</h2>
+          <div className="todoapp">
+            <Header />
+            <TodoList />
+            <Footer />
+          </div>
+        </section>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
-
 import './api/server'
+import store from './store'
+import { fetchTodos } from './features/todos/todosSlice'
+
+// 초기 todos 데이터 가져오기
+store.dispatch(fetchTodos())
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
